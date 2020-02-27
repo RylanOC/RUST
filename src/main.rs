380436793d407e0eaa8ecefc643df71fa119a386
Antonia Calia-Bogan <alfriadox@gmail.com>
2020-02-26 21:35:37 -0500

    continue wip

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,19 @@ mod web;
 #[macro_use] extern crate actix_web;
 #[macro_use] extern crate log;
 #[macro_use] extern crate serde_derive;
-#[macro_use] extern crate lazy_static;
+//#[macro_use] extern crate lazy_static;
 
 const BIND_TO: &'static str = "127.0.0.1:8000";
 const LOG_LEVEL: &'static str = "info";
 
 use std::{env, io};
 
-use log::info;
-
 use actix_files as afs;
 use actix_web::{HttpServer, App, middleware, web as a_web};
 
-use crate::web::is_up;
+use handlebars::Handlebars;
+
+use crate::web::*;
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
@@ -26,10 +26,20 @@ async fn main() -> io::Result<()> {
     env_logger::init();
     info!("Starting up.");
 
-    HttpServer::new(||
+    let mut h = Handlebars::new();
+    h.set_strict_mode(true);
+    h.register_templates_directory(".hbs", "static").unwrap();
+    let handlebars_ref = a_web::Data::new(h);
+    info!("Handlebars templates registered.");
+
+    HttpServer::new(move ||
         App::new()
-            .wrap(middleware::Logger::default()) // logger should always be last middleware added.
-            .service(a_web::resource("/is_up").to(is_up))
+            .wrap(middleware::Logger::default())
+                // logger should always be last middleware added.
+            .data(handlebars_ref.clone())
+            .service(a_web::resource("/is_up").get().to(is_up))
+            // `.get()` is a Guard -- requires GET request otherwise return 405-Method Not Allowed
+            .default_service(a_web::resource("").to(p404))
     ).bind(BIND_TO)?
         .run()
         .await
--- src/web.rs
+++ src/web.rs
@@ -1,11 +1,25 @@
 use actix_web::{HttpRequest, HttpResponse};
 use actix_web::http::Method;
+use actix_web::web::Data;
+use handlebars::Handlebars;
 
 /// Check if the website is up. Responds with 200 - OK to all GET requests.
-pub fn is_up(req: HttpRequest) -> HttpResponse {
+pub async fn is_up(req: HttpRequest) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
 
+/// 404 page and response.
+pub async fn p404(req: HttpRequest) -> HttpResponse {
     match *req.method() {
-        Method::GET => HttpResponse::Ok(),
+        Method::GET => HttpResponse::NotFound(),
+        _ => HttpResponse::MethodNotAllowed(),
+    }.finish()
+}
+
+/// Index page.
+pub async fn index(req: HttpRequest, data: Data<Handlebars<'static>>) -> HttpResponse {
+    match *req.method() {
+        Method::GET => HttpResponse::NoContent(),
         _ => HttpResponse::MethodNotAllowed(),
     }.finish()
 }
