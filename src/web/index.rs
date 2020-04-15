01599fde3ffa355e26dbf94612ba8710317eaf99
Rylan <rylanoc@gmail.com>
2020-04-14 21:32:40 -0400

    generate HTML table from arist list

--- src/web/index.rs
+++ src/web/index.rs
@@ -1,8 +1,33 @@
-use actix_web::{HttpRequest, HttpResponse};
-use actix_web::web::Data;
 use crate::app::AppState;
-use actix_web::http::Method;
+use crate::model::{Artist, Items};
 use crate::templates::Curtain;
+use actix_web::http::Method;
+use actix_web::web::Data;
+use actix_web::{HttpRequest, HttpResponse};
+
+// Constructs HTML list of recently listened to artists
+pub async fn generate_artist_list(artists: Items) -> String {
+    let mut table: String = "
+    <tr>
+        <th>Artist</th>
+        <th>Popularity</th>
+        <th>Genre</th>
+    </tr>
+    "
+    .to_owned();
+    for artist in artists.items {
+        table.push_str("<tr>");
+        table.push_str(&format!("<th>{name}</th>", name = &artist.name));
+        table.push_str(&format!(
+            "<th>{popularity}</th>",
+            popularity = &artist.popularity
+        ));
+        table.push_str(&format!("<th>{genre}</th>", genre = &artist.genres[0])); //TODO: generate comma seperated list from Vec
+        table.push_str("</tr>");
+    }
+
+    return table;
+}
 
 /// Index page.
 pub async fn index(req: HttpRequest, app_data: Data<AppState>) -> HttpResponse {
