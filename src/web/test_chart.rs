0d5e231d562cb41d9b5f340775ca12966392c621
Gabe Maayan <gabemgem@gmail.com>
2020-04-16 11:52:44 -0500

    Outputs chart to website

--- src/web/test_chart.rs
+++ src/web/test_chart.rs
@@ -14,7 +14,7 @@ pub async fn testchart(req: HttpRequest, app_data: Data<AppState>) -> HttpRespon
         Method::GET => {
             let colors = vec!["white","#1DB954","hotpink","yellow","cornflowerblue","crimson","mediumorchid"];
             let d1 = BarchartDatum::new("R-B", 7.0);
-            let d2 = BarchartDatum::new("New Wave Classical Ska", 2.0);
+            let d2 = BarchartDatum::new("New Wave", 2.0);
             let d3 = BarchartDatum::new("Sunset Groove", 18.0);
             let d4 = BarchartDatum::new("Indie Pop", 4.0);
             let data: Vec<BarchartDatum> = vec![d1,d2,d3,d4];
